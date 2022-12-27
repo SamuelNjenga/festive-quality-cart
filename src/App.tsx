175bf92c55bbd0d6ad20a58262141fd9df3fdde0
Samuel Njenga <snjenga704@gmail.com>
2022-12-27 20:49:53 +0300

    feat: set up initial folder and file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import {Navbar} from "./components/Navbar"
 function App() {
-
   return (
-    <h1>Hi</h1>
-  )
+    <>
+    <Navbar/>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,4 @@
+import {Navbar as NavbarBs} from "react-bootstrap"
+export function Navbar() {
+  return <NavbarBs className="bg-white shadow-sm mb-3">Nav</NavbarBs>;
+}
