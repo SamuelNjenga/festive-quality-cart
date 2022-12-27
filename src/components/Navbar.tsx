75e3a26e2e18dbebadcc0670fcbf6ee761b41e8f
Samuel Njenga <snjenga704@gmail.com>
2022-12-27 20:56:17 +0300

    feat: modify Navbar.tsx and add Home,Store and About links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,21 @@
-import {Navbar as NavbarBs} from "react-bootstrap"
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 export function Navbar() {
-  return <NavbarBs className="bg-white shadow-sm mb-3">Nav</NavbarBs>;
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </NavbarBs>
+  );
 }
