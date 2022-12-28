40bc563c0c3fbad98674b2e92d42401f50f806f4
Samuel Njenga <snjenga704@gmail.com>
2022-12-28 19:54:41 +0300

    feat: add ShoppingCartContext.tsx file and include shopping cart functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
-import {Navbar} from "./components/Navbar"
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
-    </>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
 }
 
